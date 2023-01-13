bd4a6dcc3005352270e06b53f944c4578c41bcad
Abdul Qadir <Abdul Qadir@YOGA-C930-ABQ.hsd1.tn.comcast.net>
2023-01-12 19:42:06 -0500

    commit day 2

---
+++ src/com/noortech/GitPush/Main.java
@@ -0,0 +1,12 @@
+package com.noortech.GitPush;
+
+public class Main {
+      public static void main(String[] args) {
+		System.out.println("Git hub work");
+		
+	}
+      public static void addtowNum(int a, int b){
+    	  System.out.println(a+b);
+    	
+      }
+      }
